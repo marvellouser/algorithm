763b81ecdf240822b3e87fa6ebcaf4873b2f8aa3
marvellous <marvellous@163.com>
2019-12-17 22:37:59 +0800

    linked list

--- index.js
+++ index.js
@@ -46,40 +46,69 @@
                     temp = temp.next;
                 }
             }
- * 
-        递归遍历：
- * 
+ *      递归遍历
+ *          function enumNode(root) {
+                if (root == null) return;
+                console.log(root.value);
+                enumNode(root.next);
+            }
  */
+ 
+
+ /**
+  * 
+  * 链表的逆置;
+  * function reverseNode(root) {
+        if (root.next.next == null) {
+            root.next.next = root;
+            return root.next;
+        }
+        var result = reverseNode(root.next);
+        root.next.next = root;
+        root.next = null;
+        return result;
+    }
+  */
 
 
-    var arr = [1,2,3,4,5,6,7,8];
+    function Node (value) {
+        this.value = value;
+        this.next = null;
+    }
+
+    var node1 = new Node(1);
+    var node2 = new Node(2);
+    var node3 = new Node(3);
+    var node4 = new Node(4);
+    var node5 = new Node(5);
+    node1.next = node2;
+    node2.next = node3;
+    node3.next = node4;
+    node4.next = node5;
+ 
+    function reverseNode(root) {
+        if (root.next.next == null) {
+            root.next.next = root;
+            return root.next;
+        }
+        var result = reverseNode(root.next);
+        root.next.next = root;
+        root.next = null;
+        return result;
+    }
+
+
+
+
+
+
     
-
-
-    // function Node (value) {
-    //     this.value = value;
-    //     this.next = null;
-    // }
-
-    // var node1 = new Node(1);
-    // var node2 = new Node(2);
-    // var node3 = new Node(3);
-    // var node4 = new Node(4);
-    // var node5 = new Node(5);
-    // node1.next = node2;
-    // node2.next = node3;
-    // node3.next = node4;
-    // node4.next = node5;
-
+    // var newNode = reverseNode(node1);
     // function enumNode(root) {
-    //     if (root == null) {
-    //         return;
-    //     }
+    //     if (root == null) return;
     //     console.log(root.value);
     //     enumNode(root.next);
     // }
-
-    
     
 
 
