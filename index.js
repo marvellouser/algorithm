091b552d5b3ebfb88c7b1be0c880c43331584030
huayun_liutao <liutao1@szhcf.com.cn>
2019-12-19 00:57:04 +0800

    tijiao

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-
-
-
 /**
  * 
  *  一维数据结构： （线性数据结构）；
@@ -53,9 +50,8 @@
                 enumNode(root.next);
             }
  */
- 
 
- /**
+/**
   * 
   * 链表的逆置;
   * function reverseNode(root) {
@@ -70,47 +66,54 @@
     }
   */
 
-
-    function Node (value) {
-        this.value = value;
-        this.next = null;
+/**
+ *
+ * 栈结构: 先进后出
+ *
+ *function Stack() {
+    this.arr = [];
+    this.push = function(value) {
+        this.arr.push(value);
+    };
+    this.pop = function() {
+        return this.arr.pop();
+    };
     }
+ */
 
-    var node1 = new Node(1);
-    var node2 = new Node(2);
-    var node3 = new Node(3);
-    var node4 = new Node(4);
-    var node5 = new Node(5);
-    node1.next = node2;
-    node2.next = node3;
-    node3.next = node4;
-    node4.next = node5;
- 
-    function reverseNode(root) {
-        if (root.next.next == null) {
-            root.next.next = root;
-            return root.next;
-        }
-        var result = reverseNode(root.next);
-        root.next.next = root;
-        root.next = null;
-        return result;
+/**
+ *
+ * 队列结构: 先进先出  (管道);
+ * 
+ * function Queue() {
+    this.arr = [];
+    this.push = function(value) {
+        this.arr.push(value);
+    };
+    this.pop = function() {
+        return this.arr.shift();
+    };
     }
+ */
 
+// function Node(value) {
+//   this.value = value;
+//   this.next = null;
+// }
 
+// var node1 = new Node(1);
+// var node2 = new Node(2);
+// var node3 = new Node(3);
+// var node4 = new Node(4);
+// var node5 = new Node(5);
+// node1.next = node2;
+// node2.next = node3;
+// node3.next = node4;
+// node4.next = node5;
 
-
-
-
-    
-    // var newNode = reverseNode(node1);
-    // function enumNode(root) {
-    //     if (root == null) return;
-    //     console.log(root.value);
-    //     enumNode(root.next);
-    // }
-    
-
-
-
-
+// var newNode = reverseNode(node1);
+// function enumNode(root) {
+//     if (root == null) return;
+//     console.log(root.value);
+//     enumNode(root.next);
+// }
