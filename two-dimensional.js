c59e4747801d288908d9467e89a79156981db5ed
marvellous <marvellous@163.com>
2019-12-22 18:19:04 +0800

    二叉树比较 && 最小生成树

--- two-dimensional.js
+++ two-dimensional.js
@@ -106,6 +106,122 @@
 
 
 
+    /**
+     * 
+     * 二叉树的比较
+     *      互换位置相等合:
+     *          function compare(root1, root2) {
+                    if (root1 == root2) return true;
+                    if (root1 == null && root2 != null || root1 != null && root2 == null) return false;
+                    if (root1.value != root2.value) return false;
+                    var left = compare(root1.left, root2.left);
+                    var right = compare(root1.right, root2.right);
+                    return compare(root1.left, root2.left) && compare(root1.right, root2.right);
+                }
+     */
+
+
+     /**
+      * 
+      * 二叉树diff算法
+      * function diffTree(root1, root2, diffList) {
+            if (root1 == root2) return diffList;
+            if (root1 == null && root2 != null) {
+                diffList.push({
+                    type: '新增',
+                    origin: null,
+                    target: root2
+                })
+            } else if (root1 != null && root2 == null) {
+                diffList.push({
+                    type: '删除',
+                    origin: root1,
+                    target: null
+                })
+            } else if (root1.value != root2.value) {
+                diffList.push({
+                    type: '修改',
+                    origin: root1,
+                    target: root2
+                })
+                diffTree(root1.left, root2.left, diffList);
+                diffTree(root1.right, root2.right, diffList);
+            } else {
+                diffTree(root1.left, root2.left, diffList);
+                diffTree(root1.right, root2.right, diffList);
+            }
+        }
+      */
+
+    function Node(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+
+    var a1 = new Node('a');
+    var b1 = new Node('b');
+    var c1 = new Node('c');
+    var d1 = new Node('d');
+    var e1 = new Node('e');
+    var f1 = new Node('f');
+    var g1 = new Node('g');
+    a1.left = c1;
+    a1.right = b1;
+    c1.left = f1;
+    c1.right = g1;
+    b1.left = d1;
+    b1.right = e1;
+
+    var a2 = new Node('a');
+    var b2 = new Node('l');
+    var c2 = new Node('c');
+    var d2 = new Node('d');
+    var e2 = new Node('e');
+    var f2 = new Node('f');
+    var g2 = new Node('j');
+    a2.left = c2;
+    a2.right = b2;
+    c2.left = f2;
+    c2.right = g2;
+    // b2.left = d2;
+    b2.right = e2;
+    e2.right = d2;
+
+    function diffTree(root1, root2, diffList) {
+        if (root1 == root2) return diffList;
+        if (root1 == null && root2 != null) {
+            diffList.push({
+                type: '新增',
+                origin: null,
+                target: root2
+            })
+        } else if (root1 != null && root2 == null) {
+            diffList.push({
+                type: '删除',
+                origin: root1,
+                target: null
+            })
+        } else if (root1.value != root2.value) {
+            diffList.push({
+                type: '修改',
+                origin: root1,
+                target: root2
+            })
+            diffTree(root1.left, root2.left, diffList);
+            diffTree(root1.right, root2.right, diffList);
+        } else {
+            diffTree(root1.left, root2.left, diffList);
+            diffTree(root1.right, root2.right, diffList);
+        }
+    }
+
+    var diffList = [];
+    diffTree(a1, a2, diffList);
+    console.log(diffList);
+
+
+
 
 
 
