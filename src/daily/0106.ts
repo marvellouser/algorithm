3469b1e4eef2c1c9554d4185aa3a843d4667e1e1
liutao <15387874583@163.com>
2026-01-07 14:19:18 +0800

    0107 有效的括号 & 逆波兰表达式求值

--- src/daily/0106.ts
+++ src/daily/0106.ts
@@ -98,4 +98,4 @@ function longestConsecutive(nums: number[]): number {
   return maxNum
 }
 
-console.log(longestConsecutive([100, 4, 200, 1, 3, 2]))
+// console.log(longestConsecutive([100, 4, 200, 1, 3, 2]))
